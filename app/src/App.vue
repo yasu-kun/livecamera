f7bbd7a0f59ef08cd7b2c9dd84e9d46a840bbb77
yasu <yasuhiro.iu@gmail.com>
2025-08-09 14:20:58 +0900

    Added sidebar

--- app/src/App.vue
+++ app/src/App.vue
@@ -5,16 +5,22 @@ import YouTubePlayerManager from './components/YouTubePlayerManager.vue'
 
 const theme = useTheme()
 const isDark = ref(theme.global.current.value.dark)
+const drawer = ref(false)
+
+const setTheme = (val) => {
+  isDark.value = !!val
+  theme.global.name.value = isDark.value ? 'dark' : 'light'
+}
 
 const toggleTheme = () => {
-  isDark.value = !isDark.value
-  theme.global.name.value = isDark.value ? 'dark' : 'light'
+  setTheme(!isDark.value)
 }
 </script>
 
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>YouTube Live Camera</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn 
@@ -25,9 +31,37 @@ const toggleTheme = () => {
         <v-icon>{{ isDark ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
       </v-btn>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" :width="300">
+      <v-toolbar flat density="comfortable">
+        <v-toolbar-title>サイドバー</v-toolbar-title>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list density="comfortable">
+        <v-list-subheader>表示</v-list-subheader>
+        <v-list-item>
+          <template #prepend>
+            <v-icon>mdi-theme-light-dark</v-icon>
+          </template>
+          <v-switch hide-details inset :model-value="isDark" @update:modelValue="setTheme" label="ダークモード"></v-switch>
+        </v-list-item>
+        <v-divider class="my-2"></v-divider>
+        <v-list-subheader>リンク</v-list-subheader>
+        <v-list-item href="https://yasu-kun.github.io/livecamera/" target="_blank">
+          <template #prepend>
+            <v-icon>mdi-open-in-new</v-icon>
+          </template>
+          公開ページ
+        </v-list-item>
+        <v-list-item href="https://github.com/yasu-kun/livecamera" target="_blank">
+          <template #prepend>
+            <v-icon>mdi-github</v-icon>
+          </template>
+          リポジトリ
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <YouTubePlayerManager />
     </v-main>
   </v-app>
 </template>
-
